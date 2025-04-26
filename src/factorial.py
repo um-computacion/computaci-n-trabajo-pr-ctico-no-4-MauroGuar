0d0489dfdd91d864eef9a8659fe678a09034f731
MauroGuar <m.guarnieri@alumno.um.edu.ar>
2025-04-26 18:02:10 -0300

    Documentado el código utilizando docstring

--- src/factorial.py
+++ src/factorial.py
@@ -1,4 +1,13 @@
 def check_num(num):
+    """Lanza una exepción si el número no es un entero no negativo.
+
+    Args:
+        num: El número a verificar.
+
+    Raises:
+        ValueError: Si el número es negativo.
+        TypeError: Si el número es de tipo flotante.
+    """
     if num < 0:
         raise ValueError
     if isinstance(num, float):
@@ -6,6 +15,18 @@ def check_num(num):
 
 
 def iterative_factorial(num: int) -> int:
+    """Calcula el factorial de un número entero no negativo de forma iterativa.
+
+    Args:
+        num (int): El número entero no negativo para el cual calcular el factorial.
+
+    Returns:
+        int: El factorial del número `num`.
+
+    Raises:
+        ValueError: Si `num` es un número negativo.
+        TypeError: Si `num` no es un número entero.
+    """
     check_num(num)
     fact_res = 1
     for i in range(1, num + 1):
@@ -14,6 +35,18 @@ def iterative_factorial(num: int) -> int:
 
 
 def recursive_factorial(num: int) -> int:
+    """Calcula el factorial de un número entero no negativo de forma recursiva.
+
+    Args:
+        num (int): El número entero no negativo para el cual calcular el factorial.
+
+    Returns:
+        int: El factorial del número `num`.
+
+    Raises:
+        ValueError: Si `num` es un número negativo.
+        TypeError: Si `num` no es un número entero.
+    """
     check_num(num)
     if num == 0:
         return 1
